59477445dd077e9ebfde9bf1e332d60814d19e73
Lihah <Lihacheva03@yandex.ru>
2024-08-06 00:32:06 +0300

    Header

--- src/components/layout/ParalaxedHeader/ParalaxedHeader.jsx
+++ src/components/layout/ParalaxedHeader/ParalaxedHeader.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './ParalaxedHeader.module.css'
 
 const ParalaxedHeader = () => {
-    window.addEventListener('scroll', e=>{
-        document.body.style.cssText = '--scrollTop: ${this.scrollY} px'
-    })
+        useEffect(() => {
+            const handleScroll = () => {
+                document.body.style.setProperty('--scrollTop', `${window.scrollY}px`);
+            };
+            window.addEventListener('scroll', handleScroll);
+
+            // Cleanup function to remove event listener when component unmounts
+            return () => {
+                window.removeEventListener('scroll', handleScroll);
+            };
+        }, []);
+
 
     return (
         <>
@@ -16,7 +25,9 @@ const ParalaxedHeader = () => {
                 <div className={styles.layer_base}/>
                 <div className={styles.layer_front}/>
             </div>
+            <div className={styles.just}>
 
+            </div>
         </>
     );
 };
